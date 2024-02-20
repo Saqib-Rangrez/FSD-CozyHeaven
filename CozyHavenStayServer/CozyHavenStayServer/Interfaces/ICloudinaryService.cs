90d71d9aac2fa059140d12563b10e7e70e6db95d
Saqib-Rangrez <saqibrangrez134@gmail.com>
2024-02-20 18:44:14 +0530

    Added File Upload Functionality

---
+++ CozyHavenStayServer/CozyHavenStayServer/Interfaces/ICloudinaryService.cs
@@ -0,0 +1,9 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace CozyHavenStayServer.Interfaces
+{
+    public interface ICloudinaryService
+    {
+        public Task<ImageUploadResult> UploadImageAsync(IFormFile file, string folder, int? height = null, int? quality = null);
+    }
+}
---
+++ CozyHavenStayServer/CozyHavenStayServer/Services/CloudinaryService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using CozyHavenStayServer.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+public class CloudinaryService : ICloudinaryService
+{
+    private readonly Cloudinary _cloudinary;
+
+    public CloudinaryService(string cloudName, string apiKey, string apiSecret)
+    {
+        var account = new Account(cloudName, apiKey, apiSecret);
+        _cloudinary = new Cloudinary(account);
+    }
+
+    public async Task<ImageUploadResult> UploadImageAsync(IFormFile file, string folder, int? height = null, int? quality = null)
+    {
+        var uploadParams = new ImageUploadParams
+        {
+            File = new FileDescription(file.FileName, file.OpenReadStream()),
+            Folder = folder,
+            UseFilename = true,
+            UniqueFilename = false,
+        };
+
+        var imageUploadResponse = await _cloudinary.UploadAsync(uploadParams);
+        return imageUploadResponse;
+    }
+}
